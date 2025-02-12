43e6bca6e3a4dc12924e26304510b68adf8760da
pkmanish2611 <pkmanish2611@gmail.com>
2025-02-12 09:55:35 +0530

    code update

--- api/index.js
+++ api/index.js
@@ -7,12 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Booking API is working!' });
-});
-
-// Routes
 app.use('/api/bookings', bookingRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
